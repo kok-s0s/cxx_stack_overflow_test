1b4721238e606509b098ffb86e147c53509f7b75
kok-s0s <kok_s0s@163.com>
2022-12-08 15:41:51 +0800

    feat: set background_task

--- main.cpp
+++ main.cpp
@@ -8,40 +8,53 @@
 
 using std::string;
 
+bool flag = true;
+
+void background_task_01() {
+  while (1) {
+    while (flag) {
+      PLOGD << "thread --- 01 --- running";
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+    while (!flag) {
+      PLOGD << "thread --- 01 --- paused";
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+  }
+}
+
+void background_task_02() {
+  while (1) {
+    while (flag) {
+      PLOGD << "thread --- 02 --- running";
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+    while (!flag) {
+      PLOGD << "thread --- 02 --- paused";
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+  }
+}
+
+void background_task_03() {}
+
 int main() {
-  plog::init(plog::debug, "cxx_test.log"); // Step2: initialize the logger
+  plog::init(plog::debug, "cxx_test.log");
 
-  // Step3: write log messages using a special macro
-  // There are several log macros, use the macro you liked the most
+  PLOGD << "Start";
 
-  PLOGD << "Hello log!";             // short macro
-  PLOG_DEBUG << "Hello log!";        // long macro
-  PLOG(plog::debug) << "Hello log!"; // function-style macro
+  ThreadPool pool(2);
 
-  // Also you can use LOG_XXX macro but it may clash with other logging
-  // libraries
-  LOGD << "Hello log!";             // short macro
-  LOG_DEBUG << "Hello log!";        // long macro
-  LOG(plog::debug) << "Hello log!"; // function-style macro
+  pool.enqueue(background_task_01);
+  pool.enqueue(background_task_02);
 
-  // create thread pool with 4 worker threads
-  ThreadPool pool(4);
-  std::vector<std::future<int>> results;
-
-  for (int i = 0; i < 8; ++i) {
-    // enqueue and store future
-    results.emplace_back(pool.enqueue([i] {
-      std::cout << "hello " << i << std::endl;
-      std::this_thread::sleep_for(std::chrono::seconds(1));
-      std::cout << "world " << i << std::endl;
-      return i * i;
-    }));
+  while (1) {
+    PLOGD << "main thread";
+    flag = !flag;
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
   }
 
-  for (auto &&result : results)
-    // get result from future
-    std::cout << result.get() << ' ';
-  std::cout << std::endl;
+  PLOGD << "End";
 
   return 0;
 }
